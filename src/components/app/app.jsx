43caabc27a6da0a9fa6f038dcdce038734be1c8f
KirillErz <erzylev@gmail.com>
2021-02-28 20:52:48 +0300

    Маршрут по тайным тропам

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,60 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import MainPage from '../main-page/main-page';
+import Login from '../login/login';
+import Favorites from '../favorites-page/favorites-page';
+import RoomPage from '../room-page/room-page';
+import NotFoundPage from '../not-found-page/not-found-page';
+import {AppRoute} from '../../utils/const';
+import browserHistory from "../../browser-history";
+import {Switch, Route, Router as BrowserRouter} from 'react-router-dom';
 
 const App = (props) => {
   const {placeCount} = props;
   return (
-    <MainPage
-      placeCount={placeCount}
-    />
+    <BrowserRouter history={browserHistory}>
+      <Switch>
+        <Route
+          exact
+          path={AppRoute.ROOT}
+          render={() => (
+            <MainPage
+              placeCount={placeCount}
+            />
+          )}
+        />
+        <Route
+          exact
+          path={AppRoute.SIGN_IN}
+          render={() => (
+            <Login
+
+            />
+          )}
+        />
+        <Route
+          exact
+          path={AppRoute.FAVORITES}
+          render={() => (
+            <Favorites
+
+            />
+          )}
+        />
+        <Route
+          exact
+          path={AppRoute.ROOM}
+          render={() => (
+            <RoomPage
+
+            />
+          )}
+        />
+        <Route>
+          <NotFoundPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
---
+++ src/components/not-found-page/not-found-page.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <p>404 Not Found</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
